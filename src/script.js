a488affa2c2be7187bc167299362d5fb94488a0b
bong-u <bong-u@users.noreply.github.com>
2023-04-05 15:58:44 +0000

    deploy: 522649c5f56431c8307b530edeadba7488e15d9f

--- src/script.js
+++ src/script.js
@@ -4,5 +4,15 @@ const toggleDarkModeHandler = () => {
     setTimeout(() => {
         document.body.classList.toggle('dark');
     }, 200);
-    
-}
+};
+const checkRecentPost = (e, recentCommitYearday) => {
+    const target = e.target;
+    // get today's yearday
+    const diff = new Date() - new Date(new Date().getFullYear(), 0, 0);
+    // convert it into day
+    const today = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+    if (today - recentCommitYearday < 2 )
+        target.classList.remove('hidden');
+    console.log(today, recentCommitYearday);
+};
