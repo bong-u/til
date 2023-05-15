56f7c1a654a8e5c1af426a1080fedd66b53c7369
bong-u <bongudev@gmail.com>
2023-05-15 17:15:27 +0900

    🔥 fix: image path problem

--- static/src/script.js
+++ static/src/script.js
@@ -1,10 +1,6 @@
 `use strict`;
 
-const toggleDarkModeHandler = () => {
-    setTimeout(() => {
-        document.body.classList.toggle('dark');
-    }, 200);
-};
+// 최근 게시물인 경우 "hidden" 클래스 제거
 const checkRecentPost = (e, recentCommitYearday) => {
     const target = e.target;
     // get today's yearday
@@ -15,3 +11,12 @@ const checkRecentPost = (e, recentCommitYearday) => {
     if (today - recentCommitYearday < 2 )
         target.classList.remove('hidden');
 };
+
+// 이미지 경로 변경
+window.onload = () => {
+    const imgElementList = document.querySelectorAll('img');
+
+    for (const imgElement of imgElementList) {
+        imgElement.src = imgElement.src.replace('/static/image', '/til-hugo/image');
+    }
+}
