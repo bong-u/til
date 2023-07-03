7f423c097659391dd2b33747fe256324a4acb652
bong-u <bongudev@gmail.com>
2023-07-03 15:44:53 +0900

    🎨 style: dark mode with custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["content/**/*.md", "layouts/**/*.html"],
   theme: {
-    extend: {},
+    colors: {
+      'dark': {
+        'bg': '#1d1e20',
+        'scroll-track': '#1d1e20',
+        'text': '#dadadb',
+        'summary': '#2e2e33',
+        'toc-hover': '#9696a5',
+        'scroll-thumb': '#52525b',
+      }
+    }
   },
   plugins: [],
 }
