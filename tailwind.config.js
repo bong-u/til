759d8d501f39f36fa4b84dc1d49f96c155958c67
bong-u <bongudev@gmail.com>
2023-08-07 17:39:12 +0900

    ♻️ refactor: css color code

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,13 @@ module.exports = {
     colors: {
       inherit: 'inherit',
       transparent: 'transparent',
-      'text': '#3f3f46', // zinc-700
-      'bg': '#e4e4e7', // zinc-200
-      'dark-text': '#d1d5db', // gray-300
-      'dark-bg': '#27272a', // zinc-800
-      'scroll-thumb': '#71717a', // zinc-500
-      'code-bg': '#1c1917', // stone-900
-      'toc-hover': '#9ca3af', // gray-400
-      'dark-toc-hover': '#71717a', // gray-500
-      'hl-bg': '#f4f4f5', // zinc-100
-      'hl-dark-bg': '#1c1917', // stone-900
-      'hl-hover-bg': '#d4d4d8', // zinc-300
-      'hl-dark-hover-bg': '#0c0a09', // stone-950
+      text: "var(--color-text)",
+      bg: "var(--color-bg)",
+      scroll_thumb: "var(--color-scroll-thumb)",
+      code_bg: "var(--color-code-bg)",
+      toc_hover: "var(--color-toc-hover)",
+      box_bg: "var(--color-box-bg)",
+      box_hover_bg: "var(--color-box-hover-bg)",
     },
     extend: {
       fontFamily: {
