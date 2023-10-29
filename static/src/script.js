99f61ceaac694bf48b0b1101d5d79a0201563bbd
bong-u <bongudev@gmail.com>
2023-10-29 23:14:21 +0900

    ⚡️ fix: comment feature with dark/light mode

--- static/src/script.js
+++ static/src/script.js
@@ -4,6 +4,7 @@ window.onload = () => {
     checkDarkMode();
     modifyImagePath();
     scrollHandler();
+    loadUtterances();
     window.addEventListener('scroll', scrollHandler);
 };
 
@@ -32,13 +33,12 @@ const modifyImagePath = () => {
 
 // 현재 다크모드 여부에 따라 class 추가
 const checkDarkMode = () => {
-
+    // dark -> light
     if (localStorage.getItem("theme") === "dark")
         document.documentElement.classList.add("dark");
     // light -> dark
     else {
         document.documentElement.classList.remove("dark");
-
     };
 }
 
@@ -57,6 +57,7 @@ const toggleDarkModeHandler = () => {
     }
 
     checkDarkMode();
+    loadUtterances();
 };
 
 
@@ -70,6 +71,7 @@ const scrollToBottom = () => {
     window.scrollTo({ top: document.body.clientHeight, behavior: 'smooth' });
 };
 
+// 스크롤 이벤트 핸들러
 const scrollHandler = () => {
     const scrollButton = document.getElementById('scrollButton');
     if (window.scrollY / (document.body.clientHeight - window.innerHeight) > 0.5) {
@@ -83,3 +85,31 @@ const scrollHandler = () => {
         scrollButton.addEventListener('click', scrollToBottom);
     }
 };
+
+// 댓글 기능
+const loadUtterances = () => {
+    const commentElement = document.getElementById('comment-box');
+
+    if (commentElement === null)
+        return;
+
+    const scriptElement = document.createElement('script');
+    scriptElement.src = "https://utteranc.es/client.js";
+    scriptElement.setAttribute('repo', "bong-u/til");
+    scriptElement.setAttribute('issue-term', "pathname");
+    scriptElement.setAttribute('label', "comment");
+    scriptElement.setAttribute('crossorigin', "anonymous");
+    scriptElement.async = true;
+
+    if (localStorage.getItem("theme") === "dark")
+        scriptElement.setAttribute('theme', "github-dark-orange");
+    else
+        scriptElement.setAttribute('theme', "github-light");
+
+    const oldChild = commentElement.firstChild;
+
+    if (oldChild)
+        commentElement.replaceChild(scriptElement, oldChild);
+    else
+        commentElement.appendChild(scriptElement);
+}
