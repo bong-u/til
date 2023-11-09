44e26b3623d6cc6b450965edd1de6587c91b0155
bong-u <bongudev@gmail.com>
2023-11-10 01:10:55 +0900

    ♻️ rename: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,14 +16,15 @@ module.exports = {
       transparent: 'transparent',
       text: "var(--color-text)",
       bg: "var(--color-bg)",
-      scroll_thumb: "var(--color-scroll-thumb)",
-      code_bg: "var(--color-code-bg)",
-      toc_hover: "var(--color-toc-hover)",
-      box_bg: "var(--color-box-bg)",
-      box_hover_bg: "var(--color-box-hover-bg)",
+      scrollbar: "var(--color-scrollbar)",
+      codebox: "var(--color-codebox)",
+      box: {
+        DEFAULT: "var(--color-box)",
+        hover: "var(--color-box-hover)",
+      },
       sidebar: "var(--color-sidebar)",
       link: "var(--color-link)",
-      quote_bar: "var(--color-quote-bar)",
+      quotebar: "var(--color-quotebar)",
     },
     extend: {
       fontFamily: {
