c3b40842e90fe0851fb7938c77ae202c194ea907
bong-u <bongudev@gmail.com>
2023-11-13 11:15:06 +0900

    ✨ feat: 최근 게시물에 N분전, N일전, N개월전 적용

--- static/script/main.js
+++ static/script/main.js
@@ -1,5 +1,9 @@
 `use strict`;
 
+window.onload = () => {
+    formatTimeDiff();
+};
+
 // 최근 게시물인 경우 "hidden" 클래스 제거
 const checkRecentPost = (e, recentCommitYearday) => {
     // 최근 게시물 기준일
@@ -12,4 +16,39 @@ const checkRecentPost = (e, recentCommitYearday) => {
 
     if (today - recentCommitYearday < NEW_THRESHOLD)
         target.classList.remove('hidden');
+};
+
+const times = [
+    { name: '년', milliSeconds: 60 * 60 * 24 * 365 },
+    { name: '개월', milliSeconds: 60 * 60 * 24 * 30 },
+    { name: '일', milliSeconds: 60 * 60 * 24 },
+    { name: '시간', milliSeconds: 60 * 60 },
+    { name: '분', milliSeconds: 60 },
+];
+
+// 시간 차이 계산
+const formatTimeDiff = () => {
+    const elements = document.getElementsByClassName('summary-date');
+    let label = '';
+
+    for (const element of elements) {
+        label = '방금 전';
+        // Date 객체 변환을 위해 " KST" 제거
+        const lastmod = new Date(element.getAttribute('lastmod').replace(" KST", ""));
+
+        const today = new Date();
+
+        const diff = (today - lastmod) / 1000;
+
+        for (const value of times) {
+            const betweenTime = Math.floor(diff / value.milliSeconds);
+
+            if (betweenTime > 0) {
+                label = `${betweenTime}${value.name} 전`;
+                break
+            }
+        }
+
+        element.innerText = label;
+    }
 };
